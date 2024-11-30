2ea353dcb12ad33ee542f958e5bf104c1e65c18e
Vitor Silva <vsilvacodeforall@gmail.com>
2024-11-30 17:32:51 +0000

    Test simpleGraph

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import com.codeforall.online.simplegraphics.graphics.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(10,10,400,400);
+        rectangle.draw();
     }
 }
